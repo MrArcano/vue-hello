73a4f9a6a07f93ef0c61b10796f740c2d1b7356f
MrArcano <salvatore-dario@hotmail.it>
2023-09-27 17:59:07 +0200

    [add]: img vue

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: "Ciao, sono VUE!"
+      message: "Ciao, sono VUE!",
+      img: `<img src="https://www.easeout.co/images/uploads/vue-logo.jpeg" alt="Vue Logo">`
     }
   }
 }).mount('#app')
